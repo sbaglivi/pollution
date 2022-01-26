0a47c6d412ce77959e74b55fa4dcf9e45fe18234
Simone <sbaglivi@gmail.com>
2022-01-26 22:45:01 +0100

    added support for uploading images and resizing them, would like to filter nsfw maybe

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ let connection = mysql.createConnection({
   password: 'enomis',
   database: 'pollution'
 })
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + Date.now());
+  }
+})
+const upload = multer({ storage: storage });
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
 app.use(express.static('views'));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
@@ -24,6 +35,24 @@ app.get("/features", (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
     res.status(200).send({ results: results });
   })
+})
+app.get('/upload', (req, res) => {
+  res.render('upload');
+})
+app.post('/upload', upload.single('evidence'), async (req, res, next) => {
+  // req.body contains the text fields, the image upload input must have name = evidence
+  // console.log(req.file);
+  const { filename: fileName } = req.file;
+
+  await sharp(req.file.path)
+    .resize(200, 200)
+    .jpeg({ quality: 90 })
+    .toFile(
+      path.resolve(req.file.destination, 'resized', fileName)
+    )
+  fs.unlinkSync(req.file.path);
+  res.redirect('/upload');
+
 })
 app.get('/search', (req, res) => {
   res.render('search');
