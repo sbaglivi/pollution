8a2733637877c0cfb60c51db078cfa9da99e25ed
Simone <sbaglivi@gmail.com>
2022-04-11 20:20:50 +0200

    added js logic to make table view of submission sortable

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,76 +1,110 @@
-const desiredFields = ['latitude', 'longitude', 'name', 'description', 'submission_date', 'author'];
 // other fields are: id, image_name, hide_author
-async function getData() {
-    let response = await fetch('api/getAllSubmissions');
-    if (response.ok) {
-        let results = response.json();
-        return results;
-    } else {
-        console.log(response);
+class TableUI {
+    constructor() {
+        this.desiredFields = ['latitude', 'longitude', 'name', 'description', 'submission_date', 'author'];
+        this.sortingOrder = { by: null, asc: null };
+        this.data = null;
+        this.tableDiv = document.getElementById('pollutedAreasTable');
+        this.table = document.createElement('table');
+        this.tableDiv.append(this.table);
+        this.tbody = document.createElement('tbody');
+        this.createTable();
     }
-}
-const parseCoordinates = (stringValue) => {
-    let floatValue = parseFloat(stringValue);
-    floatValue = floatValue.toFixed(4);
-    let processedStringValue = String(floatValue);
-    // This is probably unnecessary if I align items to the left. it's only useful if I center them and want all of them to have same length. 9 comes from 4 decimal spots, '.', 3 spots for the integer value and a possible '-'
-    // processedStringValue = processedStringValue.padStart(9, ' '); 
-    return processedStringValue;
-}
-async function createTable() {
-    let tableDiv = document.getElementById('pollutedAreasTable');
-    let data = await getData();
-    if (data.length === 0) {
-        let p = document.createElement('p');
-        p.textContent = 'There are currently no polluted areas signaled or we failed to fetch them. Try again later.'
-        tableDiv.append(td);
-        return;
-    }
-    let table = document.createElement('table');
-    createTableHead(table, data[0]);
-    let tr, td;
-    for (let row of data) {
-        tr = document.createElement('tr');
-        // for (let prop in row) {
-        for (let prop of desiredFields) {
-            td = document.createElement('td');
-            switch (prop) {
-                case 'author':
-                    td.textContent = row.hide_author ? '/' : row[prop];
-                    break;
-                case 'submission_date':
-                    let date = new Date(row[prop]);
-                    td.textContent = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`
-                    break;
-                case 'name':
-                    let a = document.createElement('a');
-                    a.textContent = row[prop];
-                    a.href = `submission/${row.id}`;
-                    td.append(a);
-                    break;
-                case 'latitude':
-                case 'longitude':
-                    td.textContent = parseCoordinates(row[prop]);
-                    td.classList.add('coordinates')
-                    break;
-                default:
-                    td.textContent = row[prop];
-            }
-            tr.append(td);
+    async createTable() {
+        this.table.append(this.createTableHead());
+        this.data = await this.getData();
+        if (this.data.length === 0) {
+            let p = document.createElement('p');
+            p.textContent = 'There are currently no polluted areas signaled or we failed to fetch them. Try again later.'
+            this.tableDiv.append(td);
+            return;
         }
-        table.append(tr);
+        this.table.append(this.tbody);
+        this.renderRows();
     }
-    tableDiv.append(table);
-}
-function createTableHead(table, row) {
-    let tr = document.createElement('tr');
-    let th;
-    // for (let prop in row) {
-    for (let prop of desiredFields) {
-        th = document.createElement('th');
-        th.textContent = prop;
-        tr.append(th);
+    renderRows() {
+        this.tbody.innerHTML = '';
+        this.tbody.append(...this.createTableRows());
     }
-    table.append(tr);
+    async getData() {
+
+        let response = await fetch('api/getAllSubmissions');
+        if (response.ok) {
+            let results = response.json();
+            return results;
+        } else {
+            console.log(response);
+        }
+    }
+    sortAndRender(clicked) {
+        this.updateSortingOrder(clicked);
+        this.sortDataByOrder();
+        this.renderRows();
+    }
+    createTableHead() {
+        let tr = document.createElement('tr');
+        let th;
+        // for (let prop in row) {
+        for (let prop of this.desiredFields) {
+            th = document.createElement('th');
+            th.textContent = prop;
+            th.addEventListener('click', this.sortAndRender.bind(this, th.textContent));
+            tr.append(th);
+        }
+        return tr;
+    }
+    createTableRows() {
+        let tr, td, rows = [];
+        for (let row of this.data) {
+            tr = document.createElement('tr');
+            // for (let prop in row) {
+            for (let prop of this.desiredFields) {
+                td = document.createElement('td');
+                switch (prop) {
+                    case 'author':
+                        td.textContent = row.hide_author ? '/' : row[prop];
+                        break;
+                    case 'submission_date':
+                        let date = new Date(row[prop]);
+                        td.textContent = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`
+                        break;
+                    case 'name':
+                        let a = document.createElement('a');
+                        a.textContent = row[prop];
+                        a.href = `submission/${row.id}`;
+                        td.append(a);
+                        break;
+                    case 'latitude':
+                    case 'longitude':
+                        td.textContent = this.parseCoordinates(row[prop]);
+                        td.classList.add('coordinates')
+                        break;
+                    default:
+                        td.textContent = row[prop];
+                }
+                tr.append(td);
+            }
+            rows.push(tr);
+            // table.append(tr);
+        }
+        return rows;
+    }
+    parseCoordinates = (stringValue) => {
+        let floatValue = parseFloat(stringValue);
+        floatValue = floatValue.toFixed(4);
+        let processedStringValue = String(floatValue);
+        // This is probably unnecessary if I align items to the left. it's only useful if I center them and want all of them to have same length. 9 comes from 4 decimal spots, '.', 3 spots for the integer value and a possible '-'
+        // processedStringValue = processedStringValue.padStart(9, ' '); 
+        return processedStringValue;
+    }
+    updateSortingOrder(clicked) {
+        console.log(`${clicked} was clicked, order was ${this.sortingOrder.asc}, ${this.sortingOrder.by}`);
+        this.sortingOrder.asc = this.sortingOrder.by === clicked ? -1 * this.sortingOrder.asc : 1;
+        this.sortingOrder.by = clicked;
+        console.log(`new order is ${this.sortingOrder.asc}, ${this.sortingOrder.by}`);
+    }
+    sortDataByOrder() {
+        this.data.sort((a, b) => this.sortingOrder.asc * (a[this.sortingOrder.by] < b[this.sortingOrder.by] ? -1 : 1));
+    }
 }
-createTable();
+let table = new TableUI();
